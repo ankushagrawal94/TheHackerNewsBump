ed8c1e0c2f9ff0b944284825044277f842e549ee
Ankush Agrawal <a3agrawa@ucsd.edu>
2014-08-14 16:52:11 -0700

    Called git commit -a to ensure the deletion of files went through

--- combined.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Executing Bash Script"
-echo "Running deleteMe.py to create hn_event_table_two"
-python deleteMe.py
-echo "Running chart.py to check the data"
-python chart.py
-echo "Complete"
