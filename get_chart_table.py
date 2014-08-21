31f3cf121baaaba1eebc6ec4263661ba6f6de8c6
Long Tran <v.long128@gmail.com>
2014-08-20 19:43:28 -0700

    New data set

--- get_chart_table.py
+++ get_chart_table.py
@@ -8,16 +8,28 @@ elapsed_time = time.time()
 
 db = MySQLdb.connect(host="localhost", # your host, usually localhost
                        user="root", # your username
-                        db="dumpDB") # name of the data base
+                        db="githubDB") # name of the data base
+
+f = open('chart_table.txt', 'w')
+f2 = open('chart_table_avg.txt', 'w')
 
 cur = db.cursor() 
 cur.execute("SELECT * FROM chart_table")
 chart_table = cur.fetchall()
 
-count = 1
+content = ''
 for each_row in chart_table:
-	print "%s\t%s\t%s\t%s\t%s\t%s" % (each_row[0], each_row[1], each_row[2], each_row[3], each_row[4], each_row[5])
-  	if count == 15:
-  		count = 0
-  		print "\t%s\t%s" % (each_row[1], each_row[2])
-  	count += 1
+	content += "%s\t%s\t%s\t%s\t%s\t%s\t0\n" % (each_row[0], each_row[1], each_row[2], each_row[3], each_row[4], each_row[5])
+  	
+
+f.write(content)
+
+content = ''
+cur.execute("SELECT * FROM chart_table_avg")
+chart_table = cur.fetchall()
+
+for each_row in chart_table:
+	content += "%s\t%s\t%s\t%s\t%s\t%s\t1\n" % (each_row[0], each_row[1], each_row[2], each_row[3], each_row[4], each_row[5])
+  	
+
+f2.write(content)
